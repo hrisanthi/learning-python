0b2b9430e209c33455040a946deca3c0e9d5cd75
hrisanthi <hrisanthi.kroi@gmail.com>
2014-01-15 13:53:45 -0500

    more cars fun

--- cars.py
+++ cars.py
@@ -10,8 +10,8 @@ carpool_capacity = cars_driven * space_in_car
 average_passengers_per_car = passengers / cars_driven
 
 
-print "There are", cars, " cars available."
-print "There are only", driver, "drivers available."
-print "We can transpaort", carpool_capacity, "people today."
-print  "We have", passengers, "to carpool today."
-print "We need to put", average_passengers_per_car, "in each car."
+print "There are",cars,"cars available."
+print "There are only",drivers,"drivers available."
+print "We can transpaort",carpool_capacity,"people today."
+print "We have %d to carpool today." %passengers
+print "We need to put %d passengers in each of the %d cars." %(average_passengers_per_car, cars_driven)
