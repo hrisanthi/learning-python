e7f8769660bb2120b0fa9ad4c6c4d07ff24cb0d2
hrisanthi <hrisanthi.kroi@gmail.com>
2014-01-15 14:17:50 -0500

    in class script

---
+++ assignment.py
@@ -0,0 +1,26 @@
+#python assignment
+
+name = "Hrisanthi"
+career = "journalist"
+age = 21
+graduation_age = age +1
+graduates = 2015
+started_school = graduates - graduation_age
+future = age * 2
+division = graduates / age
+
+
+
+
+
+print "This is %s." %name
+print name, "is", age, "years old."
+print "She will graduate in", graduates, "."
+print "She will be", age, "when she graduates."
+print name, "started school in", started_school, "."
+print "When", name, "is", future, "she hopes to be working as a", career, "."
+print "If you divide", name, "graduation year by her age the answer is", division, "."
+print "(I had to add that last line because the assignement calls for division)"
+
+
+
